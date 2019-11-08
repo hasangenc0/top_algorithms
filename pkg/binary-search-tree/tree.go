1f525359cfb1ebe69483bdebc6fddc19a6c87238
Hasan Genc <hasangencx@hotmail.com>
2019-11-09 00:04:16 +0300

    Add bst

--- pkg/binary-search-tree/tree.go
+++ pkg/binary-search-tree/tree.go
@@ -6,6 +6,7 @@ type Node struct {
 	data int
 	left *Node
 	right *Node
+	parent *Node
 }
 
 func (tree *Node) IsEmpty() bool {
@@ -18,16 +19,18 @@ func (tree *Node) Insert(data int) {
 	} else {
 		if tree.data > data {
 			if tree.left == nil {
-				tree.left = &Node{data, nil, nil}
+				tree.left = &Node{data, nil, nil, tree}
 			} else {
 				tree.left.Insert(data)
 			}
-		} else {
+		} else if tree.data < data {
 			if tree.right == nil {
-				tree.right = &Node{data, nil, nil}
+				tree.right = &Node{data, nil, nil, tree}
 			} else {
 				tree.right.Insert(data)
 			}
+		} else {
+			return
 		}
 	}
 }
@@ -36,8 +39,20 @@ func (tree Node) Delete(data int) {
 
 }
 
-func (tree Node) Search(data int) {
+func (tree Node) Search(data int) *Node {
+	if tree.IsEmpty() {
+		return nil
+	}
 
+	if tree.data == data {
+		return &tree
+	} else if tree.data > data && tree.left != nil {
+		return tree.left.Search(data)
+	} else if tree.data < data && tree.right != nil {
+		return tree.right.Search(data)
+	}
+
+	return nil
 }
 
 func (tree Node) Print() {
@@ -66,6 +81,10 @@ func main() {
 	tree.Insert(2)
 	tree.Insert(6)
 	tree.Insert(3)
+
+	if src := tree.Search(2); src != nil {
+		fmt.Println("Search Result: ", src.data)
+	}
 
 	tree.Print()
 
