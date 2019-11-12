5cf4270b535feb73cf22eff0f5dd0a8be19041b3
Hasan Genc <hasangencx@hotmail.com>
2019-11-12 14:15:35 +0300

    Add bst benchmarks

--- pkg/binary-search-tree/tree_bench.go
+++ pkg/binary-search-tree/tree_bench.go
@@ -1,1 +1,52 @@
 package binary_search_tree
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+)
+
+var tree = Node{}
+
+func InsertBenchmark(b *testing.B) {
+	min := 1
+	max := 20
+
+	for n := 0; n < b.N; n++ {
+		tree.Insert(rand.Intn(max - min) + min)
+	}
+}
+
+func SearchBenchmark(b *testing.B) {
+	min := 1
+	max := 20
+
+	for n := 0; n < b.N; n++ {
+		tree.Search(rand.Intn(max - min) + min)
+	}
+}
+
+func DeleteBenchmark(b *testing.B) {
+	min := 1
+	max := 20
+
+	for n := 0; n < b.N; n++ {
+		tree.Delete(rand.Intn(max - min) + min)
+	}
+}
+
+func Benchmark() {
+	// Insert Bench
+	insertB := testing.Benchmark(InsertBenchmark)
+	searchB := testing.Benchmark(SearchBenchmark)
+	deleteB := testing.Benchmark(DeleteBenchmark)
+
+	fmt.Print("Bst Insert Test: ")
+	fmt.Println(insertB)
+
+	fmt.Print("Bst Search Test: ")
+	fmt.Println(searchB)
+
+	fmt.Print("Bst Delete Test: ")
+	fmt.Println(deleteB)
+}
